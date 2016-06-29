2fd63e55222e18597a5693ffa905c4c3edab8439
Daniel Bowden <github@bowden.in>
2016-06-29 23:34:42 +1000

    Add an iOS example project that uses cocoapods

---
+++ iOS/Examples/CocoapodsDemo/CocoapodsDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  CocoapodsDemo
+//
+//  Copyright © 2016 AirService Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
